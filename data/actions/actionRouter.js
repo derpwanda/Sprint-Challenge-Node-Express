6ef93e7eda1a78d764d41c059a2fee8ba0bb0cb0
Wanda <doubleuevans@gmail.com>
2018-11-16 11:49:23 -0600

    CRUD in both routers; getProjectActions in projectRouter; functional

--- data/actions/actionRouter.js
+++ data/actions/actionRouter.js
@@ -1,9 +1,72 @@
 const express = require('express');
 const router = express.Router();
-const action = require('../helpers/actionModel.js');
+const actionsDb = require('../helpers/actionModel.js');
 
 //ENDPOINTS
 // /api/actions
+
+//all actions
+router.get('/', (req, res) => {
+  actionsDb.get()
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "projects not retrievable"})
+    })
+});
+
+// actions by id
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  actionsDb.get(id)
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "project information could not be retrieved"})
+    })
+});
+
+// create/add action
+router.post('/', (req, res) => {
+  actionsDb.insert(req.body)
+    .then(action => {
+      res.status(200).json(action);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "error adding action", err })
+    })
+});
+
+//update action
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  actionsDb.update(id, changes)
+    .then(action => {
+      if(action) {
+        res.status(200).json({ message: 'action updated!' })
+      } else {
+        res.status(404).json({ message: 'action not found' })
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "error updating action"})
+    })
+});
+
+//delete action
+router.delete('/:id', (req, res) => {
+  actionsDb.remove(req.params.id)
+    .then(action => {
+      res.status(200).json(action)
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'error deleting action' })
+    })
+})
+
 
 
 module.exports = router;
--- data/projects/projectRouter.js
+++ data/projects/projectRouter.js
@@ -1,9 +1,80 @@
 const express = require('express');
 const router = express.Router();
-const project = require('../helpers/projectModel.js');
+const projectsDb = require('../helpers/projectModel.js');
+const actionsDb = require('../helpers/actionModel.js');
 
 //ENDPOINTS
-// /api/actions
+// /api/projects
 
+//all projects
+router.get('/', (req, res) => {
+  projectsDb.get()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "projects not retrievable"})
+    })
+});
+
+// post by id
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  projectsDb.get(id)
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "project information could not be retrieved"})
+    })
+});
+
+// get project actions
+router.get('/:id/actions', (req, res) => {
+  const { id } = req.params;
+  projectsDb.getProjectActions(id)
+    .then(project => {
+      res.status(200).json(project)
+    })
+
+})
+
+// create/add post
+router.post('/', (req, res) => {
+  projectsDb.insert(req.body)
+    .then(project => {
+      res.status(200).json(project);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "error adding project", err })
+    })
+});
+
+//update post
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  projectsDb.update(id, changes)
+    .then(project => {
+      if(project) {
+        res.status(200).json({ message: 'project updated!' })
+      } else {
+        res.status(404).json({ message: 'project not found' })
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "error updating project"})
+    })
+});
+
+router.delete('/:id', (req, res) => {
+  projectsDb.remove(req.params.id)
+    .then(project => {
+      res.status(200).json(project)
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'error deleting project' })
+    })
+})
 
 module.exports = router;
